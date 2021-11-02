4c4f3e5e5e038b04b78257b71e4b0662d810cfa9
Jo Anna Mollman <mommollman@gmail.com>
2021-11-02 15:26:56 -0700

    set up online dictionary app

---
+++ script.js
@@ -0,0 +1,38 @@
+let input = document.querySelector('.form-control');
+const btn = document.querySelector('.btn')
+const getAWord = document.querySelector('.word-container')
+
+
+
+btn.addEventListener('click', () => {
+  console.log('the click worked')
+  getWord()
+  return getAWord.innerHTML = input.value
+})
+
+
+async function getWord() {
+  let apiKey = config.apiKey
+ const headerDetails = {
+  "headers": {
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+      "x-rapidapi-key": apiKey,
+      "accept": "application/json"
+    }
+ }
+  const url =  `https://wordsapiv1.p.rapidapi.com/words/${input.value}`
+
+  let response = await fetch(url, headerDetails)
+  let data = await response.json()
+  console.log(data)
+  input.value = ''
+  
+}
+
+
+  // let apikey = '5220347bf7msh6020c449e94f746p1b64d0jsn75f6fd17ca2b'
+
+  
+
+
+
